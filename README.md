b33f0aca7c13d6433c8543d4c825af43b83d0df3
bozzhik <bozzhik@ya.ru>
2023-12-28 16:15:55 +0300

    clone `astro` project

---
+++ README.md
@@ -0,0 +1,5 @@
+# Graphite — website for party promotion
+
+```bash
+git clone https://github.com/mbozhik/inkz.git && cd inkz && npm i && code .
+```
---
+++ tailwind.config.mjs
@@ -0,0 +1,26 @@
+const plugin = require('tailwindcss/plugin')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: 'jit',
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['SuisseIntl', 'ui-sans-serif', 'system-ui', '-apple-system', 'Roboto', 'sans-serif'],
+      },
+    },
+    screens: {
+      xl: {max: '1536px'},
+      lg: {max: '1280px'},
+      md: {max: '810px'},
+      sm: {max: '428px'},
+    },
+  },
+  plugins: [
+    // adds a `s-*` utility to apply the same width and height
+    plugin(function sizePlugin(api) {
+      api.matchUtilities({s: (value) => ({width: value, height: value})}, {values: api.theme('width')})
+    }),
+  ],
+}
