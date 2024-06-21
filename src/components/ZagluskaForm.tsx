6dd24ad5a2dfd21fa4f4a431a4d29828032a0a54
maxim <bozzhik@ya.ru>
2024-06-21 22:35:30 +0300

    refactor optimized `ZagluskaForm` react index component

--- src/components/ZagluskaForm.tsx
+++ src/components/ZagluskaForm.tsx
@@ -1,22 +1,19 @@
 import {useState} from 'react'
 import {cn} from '../lib/utils'
-import axios from 'axios'
 
 export default function HeroAction() {
   const [email, setEmail] = useState('')
   const [isSubmitted, setIsSubmitted] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false)
   const [isChecked, setIsChecked] = useState(false)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
-
+    setIsSubmitting(true)
     setIsChecked(true)
 
     const sendData = async () => {
-      const data = {
-        email,
-      }
-
+      const data = {email}
       const GOOGLE_SHEET_URL = 'https://script.google.com/macros/s/AKfycbx2tMsuPmmtzlUCs0BcfBBDL42yY3A7rNorD7IE5rNZ-mEOK779yyUerfzbGS8MDm4H/exec'
 
       try {
@@ -38,9 +35,13 @@ export default function HeroAction() {
         setIsSubmitted(true)
         setTimeout(() => {
           setIsSubmitted(false)
+          setEmail('')
+          setIsChecked(false)
         }, 2000)
       } catch (error) {
         console.error('Error:', error)
+      } finally {
+        setIsSubmitting(false)
       }
     }
 
@@ -49,7 +50,7 @@ export default function HeroAction() {
 
   const formElemStyles = {
     default: 'w-fit sm:w-full text-lg font-book px-5 py-2.5 rounded-[10px] border-2 duration-200',
-    solid: 'bg-black text-white border-transparent hover:opacity-85 ',
+    solid: 'bg-black text-white border-transparent hover:opacity-85',
     outline: 'bg-transparent text-black placeholder:text-black border-2 border-black hover:opacity-70',
   }
 
@@ -58,18 +59,20 @@ export default function HeroAction() {
       {isSubmitted ? (
         <div className={cn(formElemStyles.default, formElemStyles.solid, 'mx-auto')}>отправлено!</div>
       ) : (
-        <div className="flex justify-center gap-2 sm:flex-col sm:gap-2">
-          <input className={cn(formElemStyles.default, formElemStyles.outline)} type="text" placeholder="введите e-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
-          <button className={cn(formElemStyles.default, formElemStyles.solid)} onClick={handleSubmit}>
-            отправить
-          </button>
-        </div>
-      )}
+        <>
+          <div className="flex justify-center gap-2 sm:flex-col sm:gap-2">
+            <input className={cn(formElemStyles.default, formElemStyles.outline)} type="text" placeholder="введите e-mail" value={email} onChange={(e) => setEmail(e.target.value)} />
+            <button className={cn(formElemStyles.default, formElemStyles.solid, isSubmitting && 'opacity-60 hover:opacity-60 cursor-not-allowed')} onClick={handleSubmit} disabled={isSubmitting}>
+              {isSubmitting ? 'отправляется..' : 'отправить'}
+            </button>
+          </div>
 
-      <div className="flex items-center justify-center gap-2">
-        <input className="accent-black" type="checkbox" checked={isChecked} onChange={(e) => setIsChecked(e.target.checked)} />
-        <label className="text-sm">даю согласие на обработку персональных данных</label>
-      </div>
+          <div className="flex items-center justify-center gap-2">
+            <input className="accent-black" type="checkbox" checked={isChecked} onChange={(e) => setIsChecked(e.target.checked)} />
+            <label className="text-sm">даю согласие на обработку персональных данных</label>
+          </div>
+        </>
+      )}
     </form>
   )
 }
